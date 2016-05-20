8d3b3e9d1e4aba519ab58e22759f6a20e66a2147
Randy Coryell <coryellr@nmsu.edu>
2016-05-20 12:05:59 -0600

    end of day

--- code 20May2016.R
+++ code 20May2016.R
@@ -8,3 +8,48 @@ d
 d$gene
 write.table(d,file="cancer_genes.txt",sep="\t",quote=FALSE,row.names=FALSE,col.names=TRUE)
 e<-read.table(file="cancer_genes.txt",header=TRUE,sep="\t")
+
+## recall all info from first column of data frame
+
+d[1,]
+## access second column (factor data type)    
+d[,2]
+
+## use column name to recall
+
+d[,"gene"]
+d["gene",]
+d[1,"gene"]
+
+
+## download and access arabidopsis thaliana genome file
+
+
+## read the table of data
+
+read.table(file,header=FALSE,sep="",
+           quote="\"",dec=".",
+           numerals=c("allow.loss","warn.loss","no.loss"),
+           row.names,col.names,
+           as.is=!stringsAsFactors,na.strings="NA",
+           colClasses=NA, nrows=-1,skip=0,
+           check.names=TRUE, fill=!blank.lines.skip,
+           strip.white=FALSE,blank.lines.skip=TRUE,
+           comment.char="#",allowEscapes = FALSE,
+           flush = FALSE,
+           stringsAsFactors = default.stringsAsFactors(),
+           fileEncoding ="",encoding="unknown",text,skipNul=FALSE)
+##Write the table
+
+write.table(x,
+            file="",
+            append = FALSE,
+            quote=TRUE,
+            sep = "",
+            eol="\n",
+            na="NA",
+            dec=".",
+            row.names = TRUE,
+            col.names = TRUE,
+            qmethod = c("escape","double"))
+
