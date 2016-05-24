09e590d9305aec5e349fe36071d5f8444956eee4
Randy Coryell <coryellr@nmsu.edu>
2016-05-24 11:41:52 -0600

    what Up?/

---
+++ Code_24_May2016.R
@@ -0,0 +1,29 @@
+transcripts <- c("p2@Rab21", 
+                 "p1@Rab21")
+values <- strsplit(transcripts, "@")
+print(values)
+
+
+number ="3.74"
+pattern= "([0-9]*)\\.([0-9]*)"
+numbers <- c("3.14","0,234854","-0.345")
+matched <- regexec(pattern,numbers)
+values <- regmatches(numbers,matched)
+print(values)
+
+
+## Phone Number example
+
+pattern= "(([0-9])){3}"
+numbers <- c("(575)313-2541","575-313-2541","5753132541")
+matched <- regexec(pattern,numbers)
+values <- regmatches(numbers,matched)
+print(values)
+
+## the code above retunrs the first three of any  establish the pattenr for the whole
+
+pattern= "\\(([0-9]{3})\\)[0-9]{3}-[0-9]{4}|([0-9]{3})-[0-9]{3}-[0-9]{4}"
+numbers <- c("(575)313-2541","575-313-2541","5753132541","06051975")
+matched <- regexec(pattern,numbers)
+values <- regmatches(numbers,matched)
+print(values)
